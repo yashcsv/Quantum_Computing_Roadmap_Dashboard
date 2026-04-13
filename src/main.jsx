d2d0f127ea1292fdc8c444da92469057e56cbc61
yashcsv <yashsinghthakur69@gmail.com>
2026-04-14 01:03:55 +0530

    Fix GitHub Pages blank page deployment

--- src/main.jsx
+++ src/main.jsx
@@ -1,17 +1,17 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { HashRouter, Routes, Route } from 'react-router-dom'
 import App from './App.jsx'
 import ContactPage from './pages/ContactPage.jsx'
 import './styles/globals.css'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <Routes>
         <Route path="/" element={<App />} />
         <Route path="/contact" element={<ContactPage />} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   </StrictMode>,
 )
--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/Quantum_Computing_Roadmap_Dashboard/',
   plugins: [react()],
 })
